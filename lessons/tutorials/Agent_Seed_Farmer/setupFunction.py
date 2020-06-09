ca095e2b2d157b2d0dfe2ba5b39a7a5b3f1b3713
Vincent Lee <vlee@vlee.me.uk>
2020-06-09 02:27:03 +0100

    Initial setup function and journals

---
+++ lessons/tutorials/Agent_Seed_Farmer/setupFunction.py
@@ -0,0 +1,5 @@
+world.fill(from_me(0,0,0), from_me(3,0,6), "dirt")
+world.fill(from_me(0,1,0), from_me(3,5,6), "air")
+world.fill(from_me(1,0,1), from_me(2,0,5), "ice")
+agent.teleport(from_me(0,1,0))
+@user-code
