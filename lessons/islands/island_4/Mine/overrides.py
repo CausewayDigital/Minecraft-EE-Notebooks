e3bdf8fe54774e548f346b17d0e1fab22cc82053
Vincent Lee <vlee@vlee.me.uk>
2020-08-04 18:36:39 +0100

    Draft 1 of Diamond Mine task
    Still needs override code and grammar check

--- lessons/islands/island_4/Mine/overrides.py
+++ lessons/islands/island_4/Mine/overrides.py
@@ -1,23 +1,30 @@
-def found_iron(direction: str):
+def mark_iron(direction: str):
     if direction is not "forward":
-        say("Direction can only be 'forward'")
-        raise Exception("Direction can oly be 'forward'")
-    if agent.inspect("forward") == "iron_ore"
-        agent.move("up")
-        agent.place(1, "forward")
-        agent.move("down")
+        say("Direction can only be forward")
+        raise Exception("Direction can oly be forward")
+    if ore_detector("forward") == "iron_ore"
+        agent.place(1, "up")
     else:
-        say("Block infront of agent isn't iron ore!)
-        raise Exception("Block infront of agent isn't iron ore")
-        
-def found_diamond(direction: str):
+        say("Block infront of agent is not iron ore!")
+        raise Exception("Block infront of agent is not iron ore")
+
+def mark_diamond(direction: str):
     if direction is not "forward":
-        say("Direction can only be 'forward'")
-        raise Exception("Direction can oly be 'forward'")
-    if agent.inspect("forward") == "diamond_ore"
-        agent.move("up")
-        agent.place(1, "forward")
-        agent.move("down")
+        say("Direction can only be forward")
+        raise Exception("Direction can oly be forward")
+    if ore_detector("forward") == "diamond_ore"
+        agent.place(1, "up")
     else:
-        say("Block infront of agent isn't diamond ore!)
-        raise Exception("Block infront of agent isn't diamond ore")
+        say("Block infront of agent is not diamond ore!")
+        raise Exception("Block infront of agent is not diamond ore")
+
+def ore_detector(direction: str):
+    block = agent.inspect(direction)
+    if block == "light_gray_terracotta":
+        return "diamond_ore"
+    elif block == "orange_terracotta":
+        return "iron_ore"
+    else:
+        return block
+
+@user-code
