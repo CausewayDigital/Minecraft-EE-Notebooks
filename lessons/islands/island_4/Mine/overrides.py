90cd73389ba41be876b24ccc7de621972f40677f
Vincent Lee <vlee@vlee.me.uk>
2020-08-05 15:41:06 +0100

    Update override code

--- lessons/islands/island_4/Mine/overrides.py
+++ lessons/islands/island_4/Mine/overrides.py
@@ -2,18 +2,21 @@ def mark_diamond(direction: str):
     if direction is not "forward":
         say("Direction can only be forward")
         raise Exception("Direction can oly be forward")
-    if ore_detector("forward") == "diamond_ore"
-        agent.place(1, "up")
+    if ore_detector("forward") is not "air":
+        agent.place(2, "up")
     else:
-        say("Block infront of agent is not diamond ore!")
-        raise Exception("Block infront of agent is not diamond ore")
+        say("Unable to detect a block!")
+        raise Exception("Unable to detect block")
         
 def mark_bin(direction: str):
     if direction is not "forward":
         say("Direction can only be forward")
         raise Exception("Direction can oly be forward")
+    if ore_detector("forward") is not "air":
+        agent.place(1, "up")
     else:
-        agent.place(2, "up")
+        say("Unable to detect a block!")
+        raise Exception("Unable to detect block")
 
 def check_ore(direction: str):
     block = agent.inspect(direction)
