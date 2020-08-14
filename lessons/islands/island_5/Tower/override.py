88f16a968ebf57dd7083a1ad39e4d7eab8047bb0
Vincent Lee <vlee@vlee.me.uk>
2020-08-14 17:27:56 +0100

    Draft 1 of towers task

--- lessons/islands/island_5/Tower/override.py
+++ lessons/islands/island_5/Tower/override.py
@@ -1,3 +1,5 @@
+import time
+
 def draw_square(size: int):
     for direction in ["forward","right","back","left"]:
         for position in range(0,size):
@@ -6,13 +8,28 @@ def draw_square(size: int):
 
 @user-code
 
-# agent.teleport([1017,159,80], [1017,159,76])
-# build_tower()
-
-# Tower A
-agent.teleport([1026,159,71], [1026,159,66])
+agent.teleport([1018,159,79], [1018,159,76])
 build_tower()
 
-# Tower B
-agent.teleport([1008,159,71], [1008,159,66])
-build_tower()
+timer = 0
+while timer < 90:
+    # The MC Fnction palces a emerald block if the code is allowed to build the 2 support towers
+    if world.is_block([1027,154,60], "emerald_block"):
+        # Tower A
+        agent.teleport([1009,159,70], [1009,159,66])
+        build_tower()
+        # Tower B
+        agent.teleport([1027,159,70], [1027,159,66])
+        build_tower()
+        break
+    # if a nether wart block is placed, then there's an error with the tower and we raise an issue
+    if world.is_block([1027,154,60], "nether_wart_block"):
+        say("There seems to be an issue with your tower")
+        raise Exception("Issue with your build_tower, please fix")
+    else:
+        time.sleep(1)
+        timer = timer + 1
+if timer == 90:
+    say("There seems to be an issue with your tower")
+    say("Reset the tower with the builder and start again")
+    raise Exception("Issue with your build_tower, please fix")
