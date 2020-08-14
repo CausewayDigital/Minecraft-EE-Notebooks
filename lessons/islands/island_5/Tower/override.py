1ccf1314d7914986a51112119cc6a4f3d635f5e2
Vincent Lee <vlee@vlee.me.uk>
2020-08-14 14:54:45 +0100

    Tower Notebook update

--- lessons/islands/island_5/Tower/override.py
+++ lessons/islands/island_5/Tower/override.py
@@ -6,4 +6,13 @@ def draw_square(size: int):
 
 @user-code
 
+# agent.teleport([1017,159,80], [1017,159,76])
+# build_tower()
+
+# Tower A
+agent.teleport([1026,159,71], [1026,159,66])
+build_tower()
+
+# Tower B
+agent.teleport([1008,159,71], [1008,159,66])
 build_tower()
