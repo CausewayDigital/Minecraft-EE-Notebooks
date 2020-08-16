4774faf66173f77a321a544f8b21ca9fc9218282
Vincent Lee <vlee@vlee.me.uk>
2020-08-16 19:04:55 +0100

    correct spelling and add override

--- lessons/islands/island_5/Translator/override.py
+++ lessons/islands/island_5/Translator/override.py
@@ -7,13 +7,14 @@ list_of_numbers = [3, 4, 0, 6, 2, 1]
 for bit in list_of_numbers:
     block = agent.inspect_data("forward")
     num = decode(block)
-    say("DECODED #" + str(num))
+    say("Decoded bit as : {}".format(num))
     if num != bit:
-        say("Incorect bit given!")
-        raise Exception("Incorect bit! Please fix your function")
+        say("Incorrect bit given!")
+        raise Exception("Incorrect bit! Please fix your function")
         break
     else:
         world.summon("snow_golem", [1007,150,132])
+        say("Loading Next Bit")
     time.sleep(1)
         
 world.summon("snow_golem", [1014,148,132])
