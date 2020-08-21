c813a9c5e69f881a6bc4a299ac39ddb5b0b902cd
Vincent Lee <vlee@vlee.me.uk>
2020-08-21 18:19:32 +0100

    new test code for translator

--- lessons/islands/island_5/Translator/override.py
+++ lessons/islands/island_5/Translator/override.py
@@ -1,15 +1,41 @@
 import time
 
+def translate_data(block: str) -> int:
+    data_dict = {
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "D": 4,
+        "E": 5,
+        "F": 6,
+        "G": 7,
+        "H": 8,
+        "I": 9
+    }
+    return data_dict.get(block, 0)
+
 @user-code
 
 
 list_of_numbers = [3, 4, 0, 6, 2, 1]
-
+# This dict holds all the 
+variable_dict = {
+        1: "A",
+        2: "B",
+        3: "C",
+        4: "D",
+        5: "E",
+        6: "F",
+        7: "G",
+        8: "H",
+        9: "I":
+    }
+    
 for bit in list_of_numbers:
-    block = agent.inspect_data("forward")
+    block = variable_dict.get(agent.inspect_data("forward"), "Z")
     num = decode(block)
     if num == None:
-        raise Exception("No input! Please fix your function, or Reset Bit Input and try again.")
+        raise Exception("No number given! Please fix your function, or Reset Bit Input and try again.")
         break
     say("Decoded bit as : {}".format(int(num)))
     if num != bit:
