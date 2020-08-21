479676751c3a2343a272aa1ff692b0a0eb11bc2a
Vincent Lee <vlee@vlee.me.uk>
2020-08-21 15:09:06 +0100

    trying new override code

--- lessons/islands/island_5/Translator/override.py
+++ lessons/islands/island_5/Translator/override.py
@@ -8,7 +8,10 @@ list_of_numbers = [3, 4, 0, 6, 2, 1]
 for bit in list_of_numbers:
     block = agent.inspect_data("forward")
     num = decode(block)
-    say("Decoded bit as : {}".format(num))
+    if num == None:
+        raise Exception("No input! Please fix your function, or Reset Bit Input and try again.")
+        break
+    say("Decoded bit as : {}".format(int(num)))
     if num != bit:
         say("Incorrect bit given!")
         raise Exception("Incorrect bit! Please fix your function, or Reset Bit Input and try again.")
