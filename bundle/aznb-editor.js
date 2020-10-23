e12149eb027af4c9def51bf7f76262b4fa4af6df
Andrew Mulholland <gbaman@users.noreply.github.com>
2020-10-23 02:23:37 +0100

    Clean up repository, removing unneeded files

--- bundle/aznb-editor.js
+++
@@ -1,2 +0,0 @@
-webpackJsonpAzureNotebooks([5],{4489:function(e,t,o){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=o(0),r=(o.n(n),o(24)),s=o(30);class d extends n.Component{render(){const e=this.props.editorComponent;return e?n.createElement(e,this.props):null}}t.ExtendedEditor=d;t.default=Object(r.connect)(e=>({editorComponent:s.editorComponent(e)}))(d)}});
-//# sourceMappingURL=aznb-editor.js.map
